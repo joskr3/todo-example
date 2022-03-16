23211abfb9e57ea1065e53b264e4df2b8a2293a1
Josue Retamozo Vargas <josretamozovar@gmail.com>
2022-03-16 17:16:29 -0500

    todo list example angular

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { Todo } from "./Todo";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
 })
 export class AppComponent {
-  title = 'todo-list';
+  todos: Todo[] = [];
+  newTodo: string = "";
+
+  // saveTodo() {
+  //   if (this.newTodo.length > 0) {
+  //     this.todos.push({
+  //       name: this.newTodo,
+  //       isComplete: false
+  //     });
+  //     this.newTodo = '';
+  //   }
+  // }
+
+  saveTodo() {
+    if (this.newTodo) {
+      let todo = new Todo();
+      todo.name = this.newTodo;
+      todo.isComplete = true;
+      this.todos.push(todo);
+      this.newTodo = "";
+    } else {
+      alert("Por favor ingresa una tarea");
+    }
+  }
+
+  // deleteTodo(todo: Todo){
+  //   let index = this.todos.indexOf(todo);
+  //   this.todos.splice(index,1);
+  // }
+
+  done(id: number) {
+    this.todos[id].isComplete = !this.todos[id].isComplete;
+  }
+
+  remove(id: number) {
+    this.todos = this.todos.filter((_, index) => index !== id);
+  }
 }
